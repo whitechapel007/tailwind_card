5c74300c798f8b2eed297e260eb7761475122fb2
your username <ebukareed@gmail.com>
2022-03-31 18:10:07 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { allGiftCards } from "./card";
 
 function App() {
+  const [value, setValue] = useState(allGiftCards);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-wrap p-4 justify-center bg-[#F4F2F3]">
+      {value.map((val) => (
+        <div className="">
+          <main className="flex flex-row p-2">
+            <section className="p-20 bg-[#230903] text-white m-5 w-full flex flex-col justify-center items-center rounded-lg shadow-2xl">
+              <div className="text-2xl mb-2 font-bold">{val.title} </div>
+
+              <div className="grid grid-cols-2 md:grid-cols-4 grid-rows-3 ">
+                {val.currencies.map((curr) => (
+                  <div className="text-sm p-2">{curr}</div>
+                ))}
+              </div>
+            </section>
+          </main>
+        </div>
+      ))}
     </div>
   );
 }
